e14ed206b80e8058f049ed851c8b4a424d42453b
Jason <contactjasonsolomon@gmail.com>
2023-06-02 16:59:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "ec2-instance" {
   instance_type          = "t2.micro"
   ami                    = "ami-0715c1897453cabd1"
-  key_name               = "nosaj" //not add '.pem'
+  key_name               = "key_name" //do not add the '.pem'
   vpc_security_group_ids = [aws_security_group.sg_jenkins.id]
   user_data              = file("user_data.tpl")
 
